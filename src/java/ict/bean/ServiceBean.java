d52d5a1d7a6c5a172881a57b00cad2ed16bc747d
Carl1073 <luckyex145@gmail.com>
2026-04-06 20:41:34 +0800

    Update [filename] with new features

--- src/java/ict/bean/ServiceBean.java
+++ src/java/ict/bean/ServiceBean.java
@@ -15,7 +15,7 @@ import java.sql.Time;
  *
  * @author 240708635
  */
-public class ServiceBean implements Serializable{
+public class ServiceBean implements Serializable {
     private String serviceId;
     private String _custId;
     private Date date;
@@ -24,4 +24,84 @@ public class ServiceBean implements Serializable{
     private ServiceType serviceType;
     private ServiceMode serviceMode;
     private String status;
+
+    public ServiceBean() {
+    }
+
+    public ServiceBean(String serviceId, String _custId, Date date, Time time, String location, ServiceType serviceType,
+            ServiceMode serviceMode, String status) {
+        this.serviceId = serviceId;
+        this._custId = _custId;
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.serviceType = serviceType;
+        this.serviceMode = serviceMode;
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getServiceId() {
+        return serviceId;
+    }
+
+    public ServiceMode getServiceMode() {
+        return serviceMode;
+    }
+
+    public ServiceType getServiceType() {
+        return serviceType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public String get_custId() {
+        return _custId;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setServiceId(String serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public void setServiceMode(ServiceMode serviceMode) {
+        this.serviceMode = serviceMode;
+    }
+
+    public void setServiceType(ServiceType serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public void set_custId(String _custId) {
+        this._custId = _custId;
+    }
+
 }
